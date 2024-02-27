fa7dca79282f2631b5f8b58fa94b88242597e8d1
garenteeh <bmg.laurent@live.fr>
2024-02-27 14:45:53 +0100

    commit for blueprint

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 const { Sequelize } = require('sequelize')
+const  db_name= process.env.DB_NAME;
+const  db_user= process.env.DB_USER;
+const db_pwd= process.env.DB_PASSWORD;
+const db_host= process.env.DB_HOST;
+
+
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+    db_name, // Database name
+    db_user, // User
+    db_pwd, // Password
   {
-    host: '', // Host
+    host: db_host, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
