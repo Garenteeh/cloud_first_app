3cbf0ebbf01db92265637e853379f86dd0d58a7c
garenteeh <bmg.laurent@live.fr>
2024-02-27 15:31:24 +0100

    add render.yaml

--- db.js
+++ db.js
@@ -1,18 +1,10 @@
 const { Sequelize } = require('sequelize')
-const  db_name= process.env.DB_NAME;
-const  db_user= process.env.DB_USER;
-const db_pwd= process.env.DB_PASSWORD;
-const db_host= process.env.DB_HOST;
-
 
 
 // database
 const sequelize = new Sequelize(
-    db_name, // Database name
-    db_user, // User
-    db_pwd, // Password
+    process.env.DB_CONNECTION_STRING, // Connection string
   {
-    host: db_host, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
